b64c7f9d9d3cc790a2c5f54ff3bcf34a700b67f0
William F. Broderick <billbrod@gmail.com>
2023-07-07 21:40:29 -0400

    adds support for folio length
    previously, would create a single folio from the the whole pdf. now, can
    create folios of smaller length

--- rearrange_singles.py
+++ rearrange_singles.py
@@ -4,33 +4,58 @@ import sys
 import os
 import shutil
 import subprocess
+import argparse
 
-filename = sys.argv[1]
-n_pages = subprocess.check_output(f'pdftk {filename} dump_data | grep "NumberOfPages" | cut -d":" -f2',
-                                  shell=True)
-n_pages = int(n_pages.decode().strip())
-shutil.copy(filename, 'tmp.pdf')
+def main(filename, folio_length=None):
 
-# if not divisible by 4, we need to add blank pages
-if (n_pages % 4) != 0:
-    page_size = subprocess.check_output(f'pdftk {filename} dump_data | grep "PageMediaDimensions" | cut -d":" -f2',
-                                        shell=True)
-    # converting to float and back to str is just to check we got the right thing
-    page_size = [str(float(x)) for x in page_size.decode().strip().split(' ')]
-    subprocess.run(f'convert xc:none -page {"x".join(page_size)} blank.pdf', shell=True)
-    n_blank_pages = 4 - (n_pages % 4)
-    blank_str = 'B1 ' * n_blank_pages
-    subprocess.run(f'pdftk A={filename} B=blank.pdf cat A1-end {blank_str} output tmp.pdf',
-                   shell=True)
-    n_pages += n_blank_pages
-    os.remove('blank.pdf')
+    n_pages = subprocess.check_output(f'pdftk {filename} dump_data | grep "NumberOfPages" | cut -d":" -f2',
+                                      shell=True)
+    n_pages = int(n_pages.decode().strip())
 
-pages = []
-curr_page = n_pages
-for i in range(n_pages // 4):
-    pages.extend([curr_page, 2*i+1, 2*i+2, curr_page-1])
-    curr_page -= 2
+    if folio_length is not None:
+        if folio_length % 4 != 0:
+            raise ValueError("folio_length must be divisible by 4!")
+        if folio_length > n_pages:
+            raise ValueError("folio_length must be smaller than the number of pages in the pdf!")
 
-pages = ' '.join([str(x) for x in pages])
-subprocess.run(f'pdftk tmp.pdf cat {pages} output booklet.pdf', shell=True)
-os.remove('tmp.pdf')
+    shutil.copy(filename, 'tmp.pdf')
+
+    # if not divisible by 4, we need to add blank pages
+    if (n_pages % 4) != 0:
+        page_size = subprocess.check_output(f'pdftk {filename} dump_data | grep "PageMediaDimensions" | cut -d":" -f2',
+                                            shell=True)
+        # converting to float and back to str is just to check we got the right thing
+        page_size = [str(float(x)) for x in page_size.decode().strip().split(' ')]
+        subprocess.run(f'convert xc:none -page {"x".join(page_size)} blank.pdf', shell=True)
+        n_blank_pages = 4 - (n_pages % 4)
+        blank_str = 'B1 ' * n_blank_pages
+        subprocess.run(f'pdftk A={filename} B=blank.pdf cat A1-end {blank_str} output tmp.pdf',
+                       shell=True)
+        n_pages += n_blank_pages
+        os.remove('blank.pdf')
+
+    if folio_length is None:
+        folio_length = n_pages
+
+    pages = []
+    for j in range(n_pages // folio_length):
+        curr_page = (j+1) * folio_length
+        for i in range(folio_length // 4):
+            pages.extend([curr_page, j*folio_length + 2*i+1, j*folio_length + 2*i+2, curr_page-1])
+            curr_page -= 2
+
+    pages = ' '.join([str(x) for x in pages])
+    subprocess.run(f'pdftk tmp.pdf cat {pages} output booklet.pdf', shell=True)
+    os.remove('tmp.pdf')
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description=("Rearrange single page pdfs into booklet / folio order, for printing"),
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument("filename",
+                        help="Which file to rearrange")
+    parser.add_argument("--folio_length", '-l', default=None, type=int,
+                        help="Length of the folio, in pages. If None, all pages will be in single folio.")
+    args = vars(parser.parse_args())
+    main(**args)
